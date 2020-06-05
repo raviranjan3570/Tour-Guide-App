21d69bc5c321ab0a376cd9ee346afd57aaf1fd1d
Ravi Ranjan <raviranjan3570@gmail.com>
2020-06-05 23:36:03 +0530

    added activities of categories

--- app/src/main/java/com/android/tourguide/MainActivity.java
+++ app/src/main/java/com/android/tourguide/MainActivity.java
@@ -1,8 +1,11 @@
 package com.android.tourguide;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView placesTextView = findViewById(R.id.places);
+        placesTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PlacesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView restaurantsTextView = findViewById(R.id.restaurants);
+        restaurantsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView hotelsTextView = findViewById(R.id.hotels);
+        hotelsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HotelsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView eventsTextView = findViewById(R.id.events);
+        eventsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EventsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/android/tourguide/RestaurantsActivity.java
@@ -0,0 +1,14 @@
+package com.android.tourguide;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+    }
+}
