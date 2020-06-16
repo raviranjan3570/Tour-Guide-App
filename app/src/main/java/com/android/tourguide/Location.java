4bca8ef28d6ba4fef580493de0655fc19bd79b39
Ravi Ranjan <raviranjan3570@gmail.com>
2020-06-16 18:58:10 +0530

    added custom class and custom adapter

--- app/src/main/java/com/android/tourguide/Location.java
+++ app/src/main/java/com/android/tourguide/Location.java
@@ -2,19 +2,37 @@ package com.android.tourguide;
 
 public class Location {
 
-    private String mName;
+    private String mTitle;
     private String mDetail;
+    private String mAddress;
+    private String mTime;
+    private int mImageId;
 
-    public Location(String mName, String mDetail) {
-        this.mName = mName;
+    public Location(String mTitle, String mDetail, String mAddress, String mTime, int mImageId) {
+        this.mTitle = mTitle;
         this.mDetail = mDetail;
+        this.mAddress = mAddress;
+        this.mTime = mTime;
+        this.mImageId = mImageId;
     }
 
-    public String getmName() {
-        return mName;
+    public String getmTitle() {
+        return mTitle;
     }
 
     public String getmDetail() {
         return mDetail;
+    }
+
+    public String getmAddress() {
+        return mAddress;
+    }
+
+    public String getmTime() {
+        return mTime;
+    }
+
+    public int getmImageId() {
+        return mImageId;
     }
 }
--- app/src/main/java/com/android/tourguide/PlacesActivity.java
+++ app/src/main/java/com/android/tourguide/PlacesActivity.java
@@ -1,8 +1,12 @@
 package com.android.tourguide;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlacesActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class PlacesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places);
+
+        List<Location> list = new ArrayList<>();
+        list.add(new Location("Mountain View", "This is the description", "California", "8:00 - 17:00", R.drawable.container));
+
+        LocationAdapter adapter = new LocationAdapter(this, list);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
